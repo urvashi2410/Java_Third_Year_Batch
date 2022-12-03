e25abbd49b62905814c824dd234c156fbb27c5c2
urvashi2410 <urvashianand0906@gmail.com>
2022-12-03 19:55:39 +0530

    operators

---
+++ Operators/Simple.java
@@ -0,0 +1,89 @@
+// import java.io.*;
+
+// packages = functions, variable combination 
+
+class Simple{
+    public static void main(String args[]){
+        // System.out.println("Hello world");
+
+        // comment = ctrl + /  
+        /* multiline comment  */
+
+        // operators 
+        // 1. assignment operator = =, += , -=, *=, /=, 
+        // 2. Arithmetic operator = +, -, *, /, % 
+        // 3. relational operator = ==, !=, >=, >, <, <= 
+        // 4. logical operator = &&, ||, ! 
+        // 5. unary operator = -, +, ++, --, ! 
+
+        // data types 
+        // int = integers = 3,4,5, 500, 600 
+        // char = character = a, b, 7, &, * 
+        // short int = -32768 32767 
+        // long int 
+        // float = 4.56, 78.90 
+        // double = 456456456456.67 
+        // boolean = true false 
+        // byte = smallest data type 
+
+        // int x;
+        // x = 90;
+        // int x = 90;
+        // int y = 67;
+
+        // System.out.println(x);
+        // x += 5;
+        // x = x + 5;
+        // System.out.println(x);
+
+        // System.out.println(x);
+        // x %= 5;
+        // System.out.println(x);
+        // System.out.println(x+y);
+        // System.out.println(x-y);
+        // System.out.printf(null, x/y);
+        // System.out.print(x*y);
+
+
+        // if (x > 7){
+        //     System.out.println(true);
+        // }
+        // else if (x != 45){
+        //     System.out.println("!=");
+        // }
+
+    
+        // System.out.print((2 > 4) || (8 > 0));
+        // System.out.print((2 > 4) && (8 > 0));
+        // System.out.print(!(2 > 4));
+
+        // int x = 90;
+        // int z;
+        // // 90
+        // System.out.println(x);
+        // z = x++;
+        // // 90
+        // System.out.println(z);
+        // // 91
+        // z = x--;
+        // // 91
+        // System.out.println(z);
+        // z = --x;
+        // // 89
+        // System.out.println(z);
+        // z = ++x;
+        // // 90
+        // System.out.println(z);
+
+
+
+        // System.out.println(-x);
+        // System.out.println(x++);
+        // System.out.println(x);
+
+        // int x = 90;
+        // System.out.println(!true);
+    }
+}
+
+// Class object functions 
